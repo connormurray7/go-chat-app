35d22e696b8712c9fa800d1551a0db7bb6bb2620
Connor Murray <connormurray7@gmail.com>
2017-03-19 14:18:25 -0500

    almost finished with tutorial

--- src/main.go
+++ src/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	// "log"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
@@ -9,15 +9,64 @@ import (
 
 var clients = make(map[*websocket.Conn]bool)
 var broadcast = make(chan Message)
-var upgrader = websocket.Upgrader{}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 type Message struct {
 	email    string `json:"email"`
-	username string `json: "username"`
-	body     string `json: "body"`
+	username string `json:"username"`
+	body     string `json:"body"`
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+	clients[ws] = true
+
+	for {
+		var message Message
+		err := ws.ReadJSON(&message)
+		if err != nil {
+			log.Println("error: ", err)
+			delete(clients, ws)
+			break
+		}
+		broadcast <- message
+	}
+}
+
+func handleMessages() {
+	for {
+		message := <-broadcast
+		for client := range clients {
+			err := client.WriteJSON(message)
+			if err != nil {
+				log.Println("error: ", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
 }
 
 func main() {
 	fs := http.FileServer(http.Dir("../public"))
 	http.Handle("/", fs)
+
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleMessages()
+
+	log.Println("Server started on :8000")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
